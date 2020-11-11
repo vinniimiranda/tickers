603b5620fe2f5c5d114a6f6e5b676290ca6c9679
Vinicius Miranda <vinniimiranda@gmail.com>
2020-11-11 14:31:41 -0300

    fix padding and size

--- src/components/Grid/styles.ts
+++ src/components/Grid/styles.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: grid;
   text-align: center;
-  grid-template-columns: repeat(auto-fill, minmax(380px, 2fr));
+  grid-template-columns: repeat(auto-fill, minmax(350px, 2fr));
   grid-gap: 2rem;
-  padding: 1rem;
 `;
