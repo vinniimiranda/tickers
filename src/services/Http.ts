d2c60db0407903358a3097d8bef9c79557e5b23b
Vinicius Miranda <vinniimiranda@gmail.com>
2020-11-16 22:47:35 -0300

    Update http consumer

--- src/services/Http.ts
+++ src/services/Http.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const Http = axios.create({
-  baseURL: 'http://localhost:3535'
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3535' : 'https://tickers-b3.herokuapp.com/'
 })
 
 export default Http
